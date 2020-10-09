79753e30771de7259492e2d0c3a36c806a3988b1
maor <maorrocky@gmail.com>
2020-10-09 12:10:57 +0300

    started cart

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>Shop</Navbar.Brand>
+            <Navbar.Brand>Maor Shop</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
