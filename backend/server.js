50605974c97ecc8574e9f1b4dcbc9968dfe9a8d0
maor <maorrocky@gmail.com>
2020-10-14 11:16:38 +0300

    heroku

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
@@ -14,9 +15,9 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API IS RUNNING....');
-});
+// app.get('/', (req, res) => {
+//   res.send('API IS RUNNING....');
+// });
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -24,6 +25,21 @@ app.use('/api/orders', orderRoutes);
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use(notFound);
 
